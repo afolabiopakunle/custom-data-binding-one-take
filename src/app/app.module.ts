f7b00c63a3678c2680c6abc7e0e5fc111a0ff132
afolabiopakunle <opakunleafolabi@gmail.com>
2021-08-05 05:38:19 +0100

    cleaned up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CockpitComponent, DisplayComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
